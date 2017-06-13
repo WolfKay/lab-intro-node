1cd9d0368066909382b2978d0948092a0b2e7597
Karla Lobo <kay@Karlas-MacBook-Pro.local>
2017-06-13 21:32:30 +0200

    ahora si

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,35 @@
-var SortedList = function() {};
+var SortedList = function() {
 
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+
+};
+
+SortedList.prototype = Object.create(Array.prototype);
+
+SortedList.prototype.size = function() {return this.length;}
+
+SortedList.prototype.add     = function(item) {
+  this.push(item);
+  this.sort(function(a,b){return a - b});
+}
+SortedList.prototype.get     = function(pos) {
+  return this[pos-1];
+}
+SortedList.prototype.max     = function() {
+  return this[this.length-1];
+}
+SortedList.prototype.min     = function() {
+  return this[0];
+}
+SortedList.prototype.average = function() {
+  return this.sum() / this.length;
+}
+SortedList.prototype.sum     = function() {
+  var sum = 0;
+  this.forEach(function(number){
+    sum += number;
+  })
+  return sum;
+}
 
 module.exports = SortedList;
+
